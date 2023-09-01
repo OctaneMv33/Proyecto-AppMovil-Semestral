bd4ca1b3248e92c4979f00fdd5b8c60cc8769f61
Raksaza <131925590+Raksaza@users.noreply.github.com>
2023-08-31 20:30:17 -0400

    cambio.html

--- MiAsistencia/src/app/reset-password/reset-password.page.ts
+++ MiAsistencia/src/app/reset-password/reset-password.page.ts
@@ -6,29 +6,39 @@ import { Animation, AnimationController } from '@ionic/angular';
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
 })
+
 export class ResetPasswordPage implements OnInit {
+
+  email: string = '';
+  password: string = '';
 
   constructor(private animationCtrl: AnimationController) {}
 
-  async animar() {
-    const cuadro = document.querySelector('.repitpass,.pass');
-  
-    const animation: Animation = this.animationCtrl.create()
-      .addElement(document.querySelectorAll('.pass,.repitpass'))
-      .duration(1000) 
-      .iterations(1) 
-      .fromTo('opacity', '0', '1');
-      
-  
-    await animation.play();
-    }
-
-
-    
   ngOnInit() {
+  
   }
+
+  async animar() {
+    const elementosAnimar = document.querySelectorAll('.repitpass, .pass, .titulo');
+
+    const animation: Animation = this.animationCtrl.create()
+      .addElement(elementosAnimar)
+      .duration(1000)
+      .iterations(2)
+      .fromTo('opacity', '0', '1')
+      .fromTo('transform', 'translateX(0)', 'translateX(50px)');
+
+    await animation.play();
   }
+
+  limpiarCampos() {
+    this.email = '';
+    this.password = '';
+  }
+}
   
 
-  
 
+
+  
+   
