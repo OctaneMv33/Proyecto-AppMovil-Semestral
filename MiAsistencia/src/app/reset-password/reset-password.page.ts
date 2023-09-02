d73b6fb14d61abe7b019260107ea37ebad34f5b0
Raksaza <131925590+Raksaza@users.noreply.github.com>
2023-09-02 14:37:06 -0400

    cambio.html

--- MiAsistencia/src/app/reset-password/reset-password.page.ts
+++ MiAsistencia/src/app/reset-password/reset-password.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Animation, AnimationController } from '@ionic/angular';
+import { Animation, AnimationController,ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-reset-password',
@@ -12,24 +12,53 @@ export class ResetPasswordPage implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor(private animationCtrl: AnimationController) {}
+  constructor(private animationCtrl: AnimationController, private toastController: ToastController ) {}
 
   ngOnInit() {
   
   }
 
   async animar() {
-    const elementosAnimar = document.querySelectorAll('.repitpass, .pass, .titulo');
+    const elementosAnimar = document.querySelectorAll(' .titulo');
 
     const animation: Animation = this.animationCtrl.create()
       .addElement(elementosAnimar)
-      .duration(1000)
-      .iterations(2)
-      .fromTo('opacity', '0', '1')
-      .fromTo('transform', 'translateX(0)', 'translateX(50px)');
+      .duration(3500)
+      .iterations(Infinity)
+      .keyframes([
+   
+        { offset: 0.5, opacity: 0.2, transform: 'translateX(100%)' },
+        { offset: 0.501, opacity: 0, transform: 'translateX(-100%)' },
+        { offset: 0.52, opacity: 0.2, transform: 'translateX(-100%)' }
+      ]);
+      
+      
+
+      animation.direction('reverse'); 
 
     await animation.play();
+
+    this.mostrarMensajeContrasenaCambiada();
   }
+
+
+  async mostrarMensajeContrasenaCambiada() {
+    const toast = await this.toastController.create({
+      message: 'Contraseña cambiada exitosamente',
+      duration: 2000, // Duración en milisegundos del mensaje
+      position: 'bottom' // Posición del mensaje en la pantalla (puedes ajustarlo según tus preferencias)
+      
+    });
+
+    await toast.present();
+  }
+
+
+
+
+
+
+
 
   limpiarCampos() {
     this.email = '';
