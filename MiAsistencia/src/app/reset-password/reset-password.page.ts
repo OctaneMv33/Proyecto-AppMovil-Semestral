0c7a687b1d2b9c80d8094b926abf33b934df5fb9
Raksaza <131925590+Raksaza@users.noreply.github.com>
2023-09-02 18:17:10 -0400

    cambio de color inputs.html
    se agrego otro input de email

--- MiAsistencia/src/app/reset-password/reset-password.page.ts
+++ MiAsistencia/src/app/reset-password/reset-password.page.ts
@@ -6,53 +6,21 @@ import { Animation, AnimationController,ToastController } from '@ionic/angular';
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
 })
-export class ResetPasswordPage implements OnInit {
+export class ResetPasswordPage  {
 
-  // Campos para limpiar
-  email: string = '';
-  password: string = '';
 
-  constructor(
-    private animationCtrl: AnimationController,
-    private toastController: ToastController
-  ) {}
-
-  ngOnInit() {
-    // Lógica de inicialización si es necesario
+    constructor(
+      private toastController: ToastController
+    ) {}
+  
+    async mostrarMensajeContrasenaCambiada() {
+      const toast = await this.toastController.create({
+        message: 'Contraseña cambiada exitosamente',
+        duration: 2000, 
+        position: 'bottom' 
+      });
+      
+      await toast.present();
+    }
   }
-
-  // Limpia los campos de email y contraseña
-  limpiarCampos() {
-    this.email = '';
-    this.password = '';
-  }
-
-  // Realiza la animación en el título
-  async animar() {
-    const elementosAnimar = document.querySelectorAll('.titulo');
-
-    const animation: Animation = this.animationCtrl.create()
-      .addElement(elementosAnimar)
-      .duration(1000)
-      .iterations(1)
-      .fromTo('opacity', '0', '1')
-      .fromTo('transform', 'translateX(0)', 'translateX(75px)');
-
-    await animation.play();
-
-    // Muestra el mensaje de contraseña cambiada
-    this.mostrarMensajeContrasenaCambiada();
-  }
-
-  // Muestra un mensaje en la pantalla
-  async mostrarMensajeContrasenaCambiada() {
-    const toast = await this.toastController.create({
-      message: 'Contraseña cambiada exitosamente',
-      duration: 2000, 
-      position: 'bottom' 
-    });
-    
-    await toast.present();
-  }
-}
 
