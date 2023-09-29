18e2688a45b86dbe422ef8650c1db31c822d3ab4
OctaneMv33 <optimusg0602@gmail.com>
2023-09-29 20:12:05 -0300

    Implementando FireBase

--- MiAsistencia/src/environments/environment.ts
+++ MiAsistencia/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBcJXY5IhBar0sCmxiR0893WGnT8IX4rrM",
+    authDomain: "miasistencia-59de2.firebaseapp.com",
+    projectId: "miasistencia-59de2",
+    storageBucket: "miasistencia-59de2.appspot.com",
+    messagingSenderId: "1068847499372",
+    appId: "1:1068847499372:web:addb97aa339288aee23049",
+    measurementId: "G-3TBCJB2KG3"
+  }
 };
 
 /*
