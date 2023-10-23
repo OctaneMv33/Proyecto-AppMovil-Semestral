6ca60833a5ac78fd1c61e7c6935dae3ec6dbfec0
OctaneMv33 <112354808+OctaneMv33@users.noreply.github.com>
2023-10-23 00:43:49 -0300

    Cambiando cositas

--- MiAsistencia/capacitor.config.ts
+++ MiAsistencia/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'MA1.0',
   appName: 'MiAsistencia',
   webDir: 'www',
   server: {
