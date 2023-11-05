b019012ef2ccdd826641053df0f97741b3d35ef8
OctaneMv33 <112354808+OctaneMv33@users.noreply.github.com>
2023-11-05 13:35:26 -0300

    Agregados nuevos modelos para implementar la 3ra entrega
    Se cambió el nombre al modelo User y ahora se llama Estudiante. Se le añadieron 2 campos nuevos a este modelo.
    
    Se cambió el campo correo en el modelo Asistencia, a campo rut.
    
    Se crearon dos modelos nuevos: Clase y Asignatura.
    
    Asignatura representa a los ramos existentes. Dentro de estudiante existe un campo de tipo Array llamado "asignatura" del tipo Asignatura. Aquí estarán definidos los ramos que tendrá cada estudiante, y con ello enlazados a las clases definidas al principio del semestre.
    
    Y tenemos el modelo Clase, que representa todas las clases programadas del semestre. Asignatura tendrá un campo llamado "clase" que es un Array de tipo Clase, para poder saber cuantas clases tendrá una Asignatura y en qué fechas.

--- MiAsistencia/src/app/app.model.ts
+++ MiAsistencia/src/app/app.model.ts
@@ -1,13 +1,29 @@
-export interface User{
+export interface Estudiante{
     rut: number;
     dvrut: string;
     email: string;
     contrasena: string;
+    pnombre: string;
+    appaterno: string;
+    asignaturas: Asignatura[];
 }
 
 export interface Asistencia {
-    correo: string;
+    rut: string;
     fecha: string;
     asignatura: string;
     seccion: string;
+}
+
+export interface Clase{
+    fecha: string;
+    horaIni: string;
+    horaFin: string;
+    estado: number;
+}
+
+export interface Asignatura{
+    sigla: string;
+    seccion: string;
+    clases: Clase[];
 }
